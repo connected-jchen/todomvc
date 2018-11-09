b029cd6ea9164f5e52a3cd09cc182bb4f518e334
jchen <jchen@connected.io>
2018-11-09 15:18:29 -0500

    web test club 9/11

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      todos: ["foo", "bar"]
+      todos: []
     }
+  }
+  componentDidMount() {
+    // TODO: fetch api endpoint to get todo items
+    fetch('https://webtestclub-todo.herokuapp.com/todo/')
+      .then(res => res.json())
+      .then(jsonBody => {
+        console.log(jsonBody);
+        this.setState({todos: jsonBody.body});
+      });
+    
+    // TODO: set state TODO
   }
   render() {
     return (
--- src/App.test.js
+++ src/App.test.js
@@ -14,6 +14,45 @@ describe('App', () => {
     expect(wrapper.exists()).toBeTruthy();
   });
 
+  it('should call fetch api with a GET request when rendered', () => {
+    const temp = fetch;
+    fetch = jest.fn();
+
+    shallow(<App />);
+    expect(fetch).toBeCalled();
+
+    fetch = temp;
+  })
+
+  it('should call fetch api with correct URL when rendered', () => {
+    const temp = fetch;
+    fetch = jest.fn();
+
+    shallow(<App/>);
+    expect(fetch.mock.calls[0][0]).toEqual('URL');
+
+    fetch = temp;
+  })
+
+  it('should set the correct state into the component', (done) => {
+    const temp = fetch;
+    fetch = jest.fn(() => Promise.resolve(
+      {json: () => {
+        Promise.resolve({
+        "message":"Ok!", "body":[{"id":50,"todo":"buy milk"}]}
+      )
+      done();
+    }
+          
+        }
+    ));
+
+    const wrapper = shallow(<App/>);
+    expect(wrapper.state()).toEqual({todos: ["buy milk"]})
+
+    fetch = temp;
+  })
+
   describe('when a new item is added', () => {
     const newItem = 'new todo';
 
