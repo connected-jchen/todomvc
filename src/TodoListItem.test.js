2810255ea171900ef696323b31f39e0eadb14cde
jchen <jchen@connected.io>
2019-03-08 10:25:26 -0500

    update instructions for test files

--- src/TodoListItem.test.js
+++ src/TodoListItem.test.js
@@ -3,23 +3,58 @@ import TodoListItem from './TodoListItem';
 import { shallow } from 'enzyme';
 
 describe('TodoListItem', () => {
-    const defaultProps = {label: "alex"};
-    const wrapper = shallow(<TodoListItem {...defaultProps} />)
+    const defaultProps = { label: "alex" };
 
     it('should render a button with caption "complete" on the right side of each todo item', () => {        
+        const wrapper = shallow(<TodoListItem {...defaultProps} />)
         const todoItem = wrapper.find('.complete');
+        
         expect(todoItem).toHaveLength(1);
         expect(todoItem.text()).toEqual('complete');
     })
 
     // TODO
-    it('should call handleTodoComplete callback with the todo item whe the "complete" btn is clicked', () => { })
+    it('should call handleTodoComplete callback with the todo item whe the "complete" btn is clicked', () => {
+        // Arrange        
+        // Act
+        // Assert
+        // Watch it fail
+        // Fix
+    });
+
     // TODO
-    it('should render a checkbox on the left side of each todo item', () => { });
+    it('should render a checkbox on the left side of each todo item', () => {
+        // Arrange
+        // Act
+        // Assert
+        // Watch it fail
+        // Fix
+    });
+
     // TODO
-    it('should render a button with caption "complete selected" on the top of the todo items list', () => { });
+    it('should render a button with caption "complete selected" on the top of the todo items list', () => {
+        // Arrange
+        // Act
+        // Assert
+        // Watch it fail
+        // Fix
+    });
+
     // TODO
-    it('should call handleTodoComplete callback with all the selected todo items when the "complete selected" btn is clicked', () => { })
+    it('should call handleTodoComplete callback with all the selected todo items when the "complete selected" btn is clicked', () => {
+        // Arrange
+        // Act
+        // Assert
+        // Watch it fail
+        // Fix
+    });
+
     // TODO
-    it('should not call handleTodoComplete callback when the "complete selected" btn is clicked and there are no selected todos', () => { })
+    it('should not call handleTodoComplete callback when the "complete selected" btn is clicked and there are no selected todos', () => {
+        // Arrange
+        // Act
+        // Assert
+        // Watch it fail
+        // Fix
+    });
 });
