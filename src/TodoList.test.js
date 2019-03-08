da8e5a31128b1dcf53ae5f8c1d485567af43f1be
jchen <jchen@connected.io>
2019-03-08 11:07:52 -0500

    setup for wtc

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -3,21 +3,79 @@ import TodoList from './TodoList';
 import TodoListItem from './TodoListItem';
 import { shallow } from 'enzyme';
 
-const defaultProps = {
-    todos: ['foo', 'bar']
-};
 
 describe('TodoList', () => {
 
-    const wrapper = shallow(<TodoList {...defaultProps} />)
+    const defaultProps = { todos: ['foo', 'bar'] };
 
-    it('renders without crashing', () => {
-        expect(wrapper.exists()).toBeTruthy();
+    describe('when it is rendered', () => {
+        // Arrange
+        const wrapper = shallow(<TodoList {...defaultProps} />)
+
+        it('should render without crashing', () => {
+            expect(wrapper.exists()).toBeTruthy();
+        });
+
+        it('should render two todo items', () => {
+            expect(wrapper.find(TodoListItem)).toHaveLength(2);
+        });
+
+        it('should render first todo item with label = foo', () => {
+            expect(wrapper.find(TodoListItem).at(0).props().label).toEqual('foo');
+        });
+
+        it('should render first todo item with label = bar', () => {
+            expect(wrapper.find(TodoListItem).at(1).props().label).toEqual('bar');
+        });
+
+        // TODO:
+        it('should render two checkboxes with className "todo-checkbox"', () => {
+            // Assert
+            // Watch it fail
+            // Fix
+        });
+
+        // TODO:
+        it('should render a button with className "complete-selected-btn"', () => {
+            // Assert
+            // Watch it fail
+            // Fix
+        });
     });
 
-    it('renders todo items with two TodoListItem', () => {
-        expect(wrapper.find(TodoListItem)).toHaveLength(2);
-        expect(wrapper.find(TodoListItem).at(0).props()).toEqual({'label': 'foo'});
-        expect(wrapper.find(TodoListItem).at(1).props()).toEqual({'label': 'bar'});
+    describe('when first TodoItem onComplete handler is fired', () => {
+        // Arrange
+        // Act
+
+        // TODO:
+        it('should fire onComplete with an array containing the todo item', () => {
+            // Assert
+            // Watch it fail
+            // Fix
+        });
+    });
+
+    describe('when the first checkbox onChange handler is fired', () => {
+        // Arrange
+        // Act
+
+        // TODO:
+        it('should set the first checkbox checked prop to true', () => {
+            // Assert
+            // Watch it fail
+            // Fix
+        });
+    });
+
+    describe('when complete selected button onClick handler is fired and there are two checkboxes checked', () => {
+        // Arrange
+        // Act
+
+        //TODO:
+        it('should fire onComplete with an array containing the two todo items', () => {
+            // Assert
+            // Watch it fail
+            // Fix
+        })
     });
 });
--- src/TodoListItem.test.js
+++ src/TodoListItem.test.js
@@ -2,59 +2,40 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import { shallow } from 'enzyme';
 
+
 describe('TodoListItem', () => {
-    const defaultProps = { label: "alex" };
+    const defaultProps = { label: 'alex' };
 
-    it('should render a button with caption "complete" on the right side of each todo item', () => {        
+    describe('when it is rendered', () => {
+        // Arrange
         const wrapper = shallow(<TodoListItem {...defaultProps} />)
-        const todoItem = wrapper.find('.complete');
-        
-        expect(todoItem).toHaveLength(1);
-        expect(todoItem.text()).toEqual('complete');
-    })
+        const completeBtn = wrapper.find('.complete-btn');
 
-    // TODO
-    it('should call handleTodoComplete callback with the todo item whe the "complete" btn is clicked', () => {
-        // Arrange        
-        // Act
-        // Assert
-        // Watch it fail
-        // Fix
+        it('should render without crashing', () => {
+            expect(wrapper.exists()).toBeTruthy();
+        });
+
+        it('should render a button with className "complete-btn"', () => {
+            expect(completeBtn).toHaveLength(1);
+        });
+
+        it('should render the complete button with text "complete"', () => {
+            expect(completeBtn.text()).toEqual('complete');
+        });
     });
 
-    // TODO
-    it('should render a checkbox on the left side of each todo item', () => {
+    describe('when the complete button onClick handler is fired', () => {
         // Arrange
-        // Act
-        // Assert
-        // Watch it fail
-        // Fix
+        const wrapper = shallow(<TodoListItem {...defaultProps} />)
+        const completeBtn = wrapper.find('.complete-btn');
+        // TODO: Act
+
+        // TODO:
+        it('should fire onComplete callback', () => {
+            // Assert
+            // Watch it fail
+            // Fix
+        });
     });
 
-    // TODO
-    it('should render a button with caption "complete selected" on the top of the todo items list', () => {
-        // Arrange
-        // Act
-        // Assert
-        // Watch it fail
-        // Fix
-    });
-
-    // TODO
-    it('should call handleTodoComplete callback with all the selected todo items when the "complete selected" btn is clicked', () => {
-        // Arrange
-        // Act
-        // Assert
-        // Watch it fail
-        // Fix
-    });
-
-    // TODO
-    it('should not call handleTodoComplete callback when the "complete selected" btn is clicked and there are no selected todos', () => {
-        // Arrange
-        // Act
-        // Assert
-        // Watch it fail
-        // Fix
-    });
 });
